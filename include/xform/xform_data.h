7000ef2f2371b147785bd39cf92cc7d15778c429
John Viega <l33th4@h4x0r.org>
2026-06-10 06:05:56 -0400

    xform: store kargs metadata in dynamic dict

--- include/xform/xform_data.h
+++ include/xform/xform_data.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// Canonical definition of ncc_xform_data_t and ncc_meta_table_t.
+// Canonical definition of ncc_xform_data_t.
 //
 // All transform files include this header instead of re-declaring
 // layout-compatible struct copies.  The struct is allocated in ncc.c
@@ -8,17 +8,6 @@
 
 #include "xform/xform_template.h"
 #include "lib/dict.h"  // ncc_dict_t used in ncc_xform_data_t
-
-#define NCC_META_TABLE_SIZE 256
-
-typedef struct {
-    char *key;
-    void *value;
-} ncc_meta_entry_t;
-
-typedef struct {
-    ncc_meta_entry_t entries[NCC_META_TABLE_SIZE];
-} ncc_meta_table_t;
 
 typedef enum {
     NCC_GC_STACK_ROOT_PARAM,
@@ -57,7 +46,7 @@ typedef struct {
     const char                *compiler;
     const char                *input_file;
     const char                *constexpr_headers;
-    ncc_meta_table_t           func_meta;
+    ncc_dict_t                 func_meta;
     ncc_dict_t                 option_meta;
     ncc_dict_t                 option_decls;
     ncc_dict_t                 generic_struct_decls;
